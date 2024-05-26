10c195f77af2f87adf42f500529430e750f18c86
Björn Fahller <bjorn@fahller.se>
2024-05-26 08:29:30 +0200

    Added copyright blurb so source files

--- check_code_formatting.sh
+++ check_code_formatting.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+#
+# columnist C++23 ECS (Entity Component System) library
+#
+# Copyright (C) Björn Fahller
+#
+#  Use, modification and distribution is subject to the
+#  Boost Software License, Version 1.0. (See accompanying
+#  file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt)
+#
+# Project home: https://github.com/rollbear/columnist
+#
+
 git ls-files '*.[ch]pp'|xargs clang-format-18 --dry-run -Werror
 exit $?
--- include/columnist/type_utils.hpp
+++ include/columnist/type_utils.hpp
@@ -1,3 +1,16 @@
+/*
+ * columnist C++23 ECS (Entity Component System) library
+ *
+ * Copyright (C) Björn Fahller
+ *
+ *  Use, modification and distribution is subject to the
+ *  Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * Project home: https://github.com/rollbear/columnist
+ */
+
 #ifndef COLUMNIST_TYPE_UTILS_HPP
 #define COLUMNIST_TYPE_UTILS_HPP
 
